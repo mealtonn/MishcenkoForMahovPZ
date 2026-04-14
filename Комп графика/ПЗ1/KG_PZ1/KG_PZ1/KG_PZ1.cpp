cc9ecc4705e7e68832d4db1a8aa4a2d379f1f3df
mealtonn <2balagan.balagan@gmail.com>
2026-04-14 23:18:08 +0300

    14.04
    PZ for Mahov

---
+++ Комп графика/ПЗ1/KG_PZ1/KG_PZ1/KG_PZ1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    double num1, num2;
+
+    cout << "Введите первое число: ";
+    cin >> num1;
+    cout << "Введите второе число: ";
+    cin >> num2;
+
+    double x = max(num1, num2);
+
+    double result = log(pow(x, 2));
+
+    cout << "Большее число: " << x << endl;
+    cout << "Результат F(x) = ln(x^2): " << result << endl;
+
+    return 0;
+}
---
+++ Комп графика/ПЗ2/KG_PZ.2/KG_PZ.2/KG_PZ.2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double calculateFunction(double x) {
+    return log(pow(x, 2));
+}
+
+int main() {
+    double number;
+    int choice;
+
+    do {
+        cout << "Введите число x: ";
+        cin >> number;
+
+        double result = calculateFunction(number);
+        cout << "Результат F(x) = " << result << endl;
+
+        cout << "Продолжить вычисления? (1 - да, 0 - нет): ";
+        cin >> choice;
+
+    } while (choice != 0);
+
+    return 0;
+}
